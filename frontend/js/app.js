dc458cfec1c0ae1211fe1234632da99afb0b5910
guzmandev <jguzmanpenagos@gamil.com>
2026-08-19 18:45:19 -0500

    finalizacion del front V1

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -6,9 +6,17 @@ const actividades = actividadesGuardadas
     : [];
 
 actividades.forEach(function (actividad) {
-    console.log(actividad.actividad);
+    console.log(actividad.actividad + ": " + actividad.dia, actividad.horaInicio + "-" + actividad.horaFin);
 });
 
+
+
+const botonLimpiar = document.getElementById("limpiar");
+
+botonLimpiar.addEventListener("click", function () {
+    localStorage.removeItem("actividades");
+    location.reload();
+});
 
 
 
@@ -29,37 +37,42 @@ activityForm.addEventListener("submit", function (event) {
         color: color
     };
 
+    const nuevoInicio = parseInt(nuevaActividad.horaInicio);
+    const nuevoFin = parseInt(nuevaActividad.horaFin);
+    let conflictoHora = nuevoFin <= nuevoInicio;
+    let conflicto = false;
+    actividades.forEach(function (actividadExistente) {
+        if (actividadExistente.dia === nuevaActividad.dia) {
+            const inicioExistente = parseInt(actividadExistente.horaInicio);
+            const finExistente = parseInt(actividadExistente.horaFin);
 
-    let conflicto=false;
-    actividades.forEach(function(actividadExistente){
-        if(actividadExistente.dia===nuevaActividad.dia){
-            const nuevoInicio=parseInt(nuevaActividad.horaInicio);
-            const nuevoFin=parseInt(nuevaActividad.horaFin);
-        
-            const inicioExistente=parseInt(actividadExistente.horaInicio);
-            const finExistente=parseInt(actividadExistente.horaFin);
-
-            if(nuevoInicio<finExistente && nuevoFin>inicioExistente){
-                conflicto=true
+            if (nuevoInicio < finExistente && nuevoFin > inicioExistente) {
+                conflicto = true
+            }
+            if (nuevoFin <= nuevoInicio) {
+                conflictoHora = true;
             }
         }
-
-
     });
 
-    if(!conflicto){
+    if (conflictoHora) {
+        mostrarAlerta(
+            "La hora de finalización debe ser posterior a la hora de inicio."
+        );
+
+    } else if (conflicto) {
+        mostrarAlerta(
+            "No se puede agregar la actividad porque existe un conflicto de horario."
+        );
+    } else {
         actividades.push(nuevaActividad);
-    }else{
-        alert("No se puede agregar la actividad porque existe un conflicto de horario.");
+        mostrarActividades();
+
+        const actividadJSON = JSON.stringify(actividades);
+        localStorage.setItem("actividades", actividadJSON);
     }
-    
-
-    const actividadJSON = JSON.stringify(actividades);
-
-    localStorage.setItem("actividades", actividadJSON);
 
 
-    console.log(actividades)
 });
 
 
@@ -76,17 +89,51 @@ const dias = [
 
 const filas = document.querySelectorAll("tbody tr");
 
-actividades.forEach(function (actividad) {
-    filas.forEach(function (fila) {
-        const hora = parseInt(fila.querySelector("th").textContent);
-        const inicio=parseInt(actividad.horaInicio);
-        const fin=parseInt(actividad.horaFin);
-        if (hora >= inicio && hora <= fin) {
-            const celdas = fila.querySelectorAll("td");
-            const indiceDia = dias.indexOf(actividad.dia);
-            const celda = celdas[indiceDia];
+function mostrarActividades() {
+    actividades.forEach(function (actividad) {
+        filas.forEach(function (fila) {
+            const hora = parseInt(fila.querySelector("th").textContent);
+            const inicio = parseInt(actividad.horaInicio);
+            const fin = parseInt(actividad.horaFin);
+            const duracion = fin - inicio;
+            if (hora > inicio && hora < fin) {
+                const celda = fila.querySelector(`[data-day="${actividad.dia}"]`);
+
+                if (celda) {
+                    celda.remove();
+                }
+            }
+            if (hora === inicio) {
+                const celda = fila.querySelector(`[data-day="${actividad.dia}"]`);
+
                 celda.textContent = actividad.actividad;
-                celda.style.background=actividad.color;
-        }
+                celda.style.textAlign = "center";
+                celda.style.verticalAlign = "middle";
+                celda.rowSpan = duracion;
+                celda.style.background = actividad.color;
+            }
+
+        });
     });
+}
+function mostrarAlerta(mensaje) {
+    const alerta = document.getElementById("custom-alert");
+    const mensajeAlerta = document.getElementById("alert-message");
+
+    mensajeAlerta.textContent = mensaje;
+
+    alerta.classList.add("show");
+}
+const alerta = document.getElementById("custom-alert");
+const botonCerrar = document.getElementById("alert-close");
+const botonOk = document.getElementById("alert-ok");
+
+botonCerrar.addEventListener("click", function () {
+    alerta.classList.remove("show");
 });
+
+botonOk.addEventListener("click", function () {
+    alerta.classList.remove("show");
+});
+
+mostrarActividades();
